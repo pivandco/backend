a8d47098f5d12ed1fe5f4e062d4865c8876693d8
javabird25 <shockck84@gmail.com>
2023-02-18 23:32:19 +0500

    Product: add navigational properties for ProductCategory

--- MerchandiseApi/Product.cs
+++ MerchandiseApi/Product.cs
@@ -5,4 +5,7 @@ public sealed class Product
     public int Id { get; set; }
     public required string Name { get; set; }
     public required decimal Price { get; set; }
+    
+    public ProductCategory? ProductCategory { get; set; }
+    public int? ProductCategoryId { get; set; }
 }
