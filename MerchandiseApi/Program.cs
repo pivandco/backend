c81daa530ef9d4e9f798acd903ec049d6668638d
javabird25 <shockck84@gmail.com>
2023-02-18 23:33:39 +0500

    Implement product category support

--- MerchandiseApi/Program.cs
+++ MerchandiseApi/Program.cs
@@ -12,27 +12,36 @@ var app = builder.Build();
 var productsApi = app.MapGroup("/api/products");
 
 productsApi.MapGet("/",
-    async (MerchandiseApiDb db, SieveProcessor sieveProcessor, int? page, int? pageSize) =>
-        await sieveProcessor.Apply(new SieveModel { Page = page, PageSize = pageSize }, db.Products.AsNoTracking())
-            .ToListAsync());
+    async (MerchandiseApiDb db, SieveProcessor sieveProcessor, int? page, int? pageSize, int? categoryId) =>
+    {
+        var sieveModel = new SieveModel { Page = page, PageSize = pageSize };
+        var products = db.Products
+            .Where(p => categoryId == null || p.ProductCategoryId == categoryId)
+            .Include(p => p.ProductCategory)
+            .Select(p => new ProductDto(p))
+            .AsNoTracking();
+
+        return await sieveProcessor.Apply(sieveModel, products).ToListAsync();
+    });
 
 productsApi.MapGet("/{id:int}", async (int id, MerchandiseApiDb db) =>
-    await db.FindAsync<Product>(id) is { } product ? Results.Ok(product) : Results.NotFound());
+    await db.FindAsync<Product>(id) is { } product ? Results.Ok(new ProductDto(product)) : Results.NotFound());
 
-productsApi.MapPost("/", async (MerchandiseApiDb db, Product product) =>
+productsApi.MapPost("/", async (MerchandiseApiDb db, ProductDto productDto) =>
 {
-    db.Products.Add(product);
+    db.Products.Add(productDto.ToProduct());
     await db.SaveChangesAsync();
-    return Results.Created($"/api/products/{product.Id}", product);
+    return Results.Created($"/api/products/{productDto.Id}", productDto);
 });
 
-productsApi.MapPut("/{id:int}", async (int id, Product newProduct, MerchandiseApiDb db) =>
+productsApi.MapPut("/{id:int}", async (int id, ProductDto newProductDto, MerchandiseApiDb db) =>
 {
     var todo = await db.Products.FindAsync(id);
     if (todo == null) return Results.NotFound();
 
-    todo.Name = newProduct.Name;
-    todo.Price = newProduct.Price;
+    todo.Name = newProductDto.Name;
+    todo.Price = newProductDto.Price;
+    todo.ProductCategoryId = newProductDto.CategoryId;
 
     await db.SaveChangesAsync();
     return Results.NoContent();
@@ -45,6 +54,43 @@ productsApi.MapDelete("/{id:int}", async (int id, MerchandiseApiDb db) =>
     db.Products.Remove(todo);
     await db.SaveChangesAsync();
     return Results.Ok(todo);
+});
+
+var categoriesApi = app.MapGroup("/api/categories");
+
+categoriesApi.MapGet("/",
+    async (MerchandiseApiDb db) => await db.ProductCategories
+        .Select(c => new ProductCategoryDto(c))
+        .ToListAsync());
+
+categoriesApi.MapGet("/{id:int}",
+    async (MerchandiseApiDb db, int id) => await db.ProductCategories.FindAsync(id) is { } category
+        ? Results.Ok(new ProductCategoryDto(category))
+        : Results.NotFound());
+
+categoriesApi.MapPost("/", async (MerchandiseApiDb db, ProductCategoryDto categoryDto) =>
+{
+    db.ProductCategories.Add(categoryDto.ToProductCategory());
+    await db.SaveChangesAsync();
+    return Results.Created($"/api/categories/{categoryDto.Id}", categoryDto);
+});
+
+categoriesApi.MapPut("/{id:int}", async (int id, MerchandiseApiDb db, ProductCategoryDto newCategoryDto) =>
+{
+    var oldCategory = await db.ProductCategories.FindAsync(id);
+    if (oldCategory == null) return Results.NotFound();
+    oldCategory.Name = newCategoryDto.Name;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+categoriesApi.MapDelete("/{id:int}", async (int id, MerchandiseApiDb db) =>
+{
+    var category = await db.ProductCategories.FindAsync(id);
+    if (category == null) return Results.NotFound();
+    db.ProductCategories.Remove(category);
+    await db.SaveChangesAsync();
+    return Results.Ok(category);
 });
 
 app.Run();
