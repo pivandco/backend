12b825a4e4ae82b0cfd7ec075b726e70294793ef
javabird25 <shockck84@gmail.com>
2023-02-19 14:35:11 +0500

    Add controller for dish tags

---
+++ RestaurantAutomation/RestaurantAutomation/Controllers/DishTagController.cs
@@ -0,0 +1,80 @@
+namespace RestaurantAutomation.Controllers;
+
+using Dto;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+[Route("[controller]")]
+[ApiController]
+public class DishTagController : ControllerBase
+{
+    private readonly RestaurantAutomationDbContext _db;
+
+    public DishTagController(RestaurantAutomationDbContext db)
+    {
+        _db = db;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<DishTagDto>> List() =>
+        await _db.DishTags.Select(t => DishTagDto.FromDishTag(t)).ToListAsync();
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<DishTagDto>> Get(int id) =>
+        await _db.DishTags.FindAsync(id) is { } tag ? DishTagDto.FromDishTag(tag) : NotFound();
+
+    [HttpPut("{id:int}")]
+    public async Task<IActionResult> Update(int id, DishTagDto dishTagDto)
+    {
+        if (id != dishTagDto.Id)
+        {
+            return BadRequest();
+        }
+
+        var dishTag = dishTagDto.ToDishTag();
+        _db.Entry(dishTag).State = EntityState.Modified;
+
+        try
+        {
+            await _db.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!DishTagExists(id))
+                return NotFound();
+            throw;
+        }
+
+        return NoContent();
+    }
+
+    private bool DishTagExists(int id)
+    {
+        return _db.DishTags.Any(e => e.Id == id);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<DishTagDto>> Create(DishTagDto dishTagDto)
+    {
+        var tag = dishTagDto.ToDishTag();
+        _db.DishTags.Add(tag);
+        await _db.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(Get), new { id = tag.Id }, tag);
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var dishTag = await _db.DishTags.FindAsync(id);
+        if (dishTag == null)
+        {
+            return NotFound();
+        }
+
+        _db.DishTags.Remove(dishTag);
+        await _db.SaveChangesAsync();
+
+        return NoContent();
+    }
+}
