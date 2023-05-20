a6eda667d1b23709002d4367d7a87cd137e39a81
Nikita Utkin <shockck84@gmail.com>
2023-05-20 15:35:41 +0500

    Subject: add Groups property

--- UniversityAccessControl/UniversityAccessControl/UniversityAccessControl/Models/Subject.cs
+++ UniversityAccessControl/UniversityAccessControl/UniversityAccessControl/Models/Subject.cs
@@ -7,4 +7,5 @@ public sealed class Subject
     public string? MiddleName { get; set; }
     public required string LastName { get; set; }
     public required DateOnly DateOfBirth { get; set; }
+    public List<Group> Groups { get; set; } = new();
 }
