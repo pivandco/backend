6e63eaa0b2d712f773ec7632fe95f85e42fd3549
Nikita Utkin <shockck84@gmail.com>
2023-05-21 12:11:58 +0500

    Swagger: add auth button

--- UniversityAccessControl/UniversityAccessControl/Program.cs
+++ UniversityAccessControl/UniversityAccessControl/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using UniversityAccessControl;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,8 +9,34 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<AccessControlDbContext>(options => options.UseSqlite($"Data Source=app.db"));
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "University Access Control API", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
+builder.Services.AddDbContext<AccessControlDbContext>(options => options.UseSqlite("Data Source=app.db"));
 
 var app = builder.Build();
 
